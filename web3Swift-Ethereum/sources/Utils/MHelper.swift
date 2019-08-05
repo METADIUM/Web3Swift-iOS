5f7c0a9a008b2948ce845176ffb1006dec1d48d9
hanjinsik <jshan@coinplug.com>
2019-08-05 17:03:52 +0900

    getEvent method modify

--- web3Swift-Ethereum/sources/Utils/MHelper.swift
+++ web3Swift-Ethereum/sources/Utils/MHelper.swift
@@ -14,7 +14,7 @@ import EthereumAddress
 public class MHelper {
 
     // getEvent
-    class func getEvent(receipt: EthereumTransactionReceipt, string: String) -> NSDictionary {
+    public class func getEvent(receipt: EthereumTransactionReceipt, string: String) -> NSDictionary {
         
         let abiEvent = Web3Utils.jsonStringToDictionary(string: string)! as NSDictionary
         
--- web3Swift.podspec
+++ web3Swift.podspec
@@ -3,7 +3,7 @@
 Pod::Spec.new do |spec|
 
   spec.name         = 'web3Swift'
-  spec.version      = '1.0.8'
+  spec.version      = '1.0.9'
   spec.summary      = 'web3 API'
 
   spec.description  = <<-DESC
